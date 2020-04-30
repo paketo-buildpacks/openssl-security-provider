79bd46ab343a4bf6feaf6543dba292c9fc8dc09b
Ben Hale <bhale@pivotal.io>
2020-04-29 22:47:09 -0700

    Test Pipeline
    Signed-off-by: Ben Hale <bhale@pivotal.io>

---
+++ scripts/test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+ROOT=$(realpath "$(dirname "${BASH_SOURCE[0]}")"/../..)
+
+if [[ -d "${ROOT}"/maven-cache && ! -d "${HOME}"/.m2 ]]; then
+  ln -s "${ROOT}"/maven-cache "${HOME}"/.m2
+fi
+
+cd "${ROOT}"/source
+
+./mvnw clean package
